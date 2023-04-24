b582144f1073c48ccda57b1304ea716670a7d4ae
shahzoda <istamovashahzoda04@gmail.com>
2023-04-24 05:12:13 +0300

    pogress

--- src/componenets/Card.js
+++ src/componenets/Card.js
@@ -1,9 +1,21 @@
 import React from "react";
+import shoppingCart from "./card.png"
 
-const Card = () =>{
+const Card = ({data, addcard}) =>{
   return(
-    <div>
-
+    <div className="card">
+    <div className="cardHeader">
+      <img src={data.url} title={data.title} className="card-image" alt="cardImage" />
+    </div>
+    <div className="cardBody">
+      <h3 className="cardTitle">{data.title}</h3>
+      <p className="cardDesc">{data.desc}</p>
+      <p className="cardPrice"><span>{data.price} Sum</span></p>
+      <button className="add-to-card" onClick={() => addcard (data)}>
+      <img src={shoppingCart} className="cartIcon"/>
+      Заказать
+      </button>
+    </div>
     </div>
   )
 }
