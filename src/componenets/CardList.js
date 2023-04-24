e4a177f675d43594d51b436521c15f57ad82117d
shahzoda <istamovashahzoda04@gmail.com>
2023-04-24 04:22:56 +0300

    pogress

--- src/componenets/CardList.js
+++ src/componenets/CardList.js
@@ -9,13 +9,32 @@ const CardList = ({products,addcart}) =>{
   setTimeout(() =>{
     setDelay(false)
   },5000)  //5sekuntga teng
- })
+ },[products])
 
 
   return(
-    <div>
-      
-    </div>
+    <>
+      {
+         delay ? < div className="loader">
+          <img src="https://pandagif.com/gifs/ysjA8HY1lA.gif"
+          alt="loader"
+          className="loader"/>
+         </div>
+         :
+         <>
+         <div className="card-list">
+          {
+             products.length === 0 ? <p>Don't fount the product</p>:
+             products.map(item => {
+              return(
+                <Card key={item.id} data={data} addcart={addcart}/>
+              )
+             })
+          }
+         </div>
+         </>
+      }
+    </>
   )
 }
 
