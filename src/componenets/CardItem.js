ecedee4486d0e25372a1b9b7029d4824010fb40b
shahzoda <istamovashahzoda04@gmail.com>
2023-04-24 03:37:34 +0300

    pogress

--- src/componenets/CardItem.js
+++ src/componenets/CardItem.js
@@ -1,10 +1,24 @@
 import React, {useState, useEffect} from "react";
+import Products from "./Products";
 
 
-const CardItem = () =>{
+const CardItem = ({ Products, changee}) =>{
   return(
+    <div className="cartItem">
+   <img src={Products.url} alt = "cartItem" className="cartItemimg" />
+   <div className="carMain">
     <div>
-
+      <p>{Products.title}</p>
+      <span>UZS{Products.price}</span>
+    </div>
+    <div>
+      <p>Число:<span>{Products.quantity}</span></p>
+      <div>
+        <button className="quantityBTN" onClick={() => changee(Products, "-")}>-</button>
+        <button className="quantityBTN" onClick={() => changee(Products, "+")}>+</button>
+      </div>
+    </div>
+   </div>
     </div>
   )
 }
