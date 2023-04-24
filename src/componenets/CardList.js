434239370f2390cf34bcc329eeda23e010c3024d
shahzoda <istamovashahzoda04@gmail.com>
2023-04-24 04:37:01 +0300

    pogress

--- src/componenets/CardList.js
+++ src/componenets/CardList.js
@@ -16,7 +16,8 @@ const CardList = ({products,addcart}) =>{
     <>
       {
          delay ? < div className="loader">
-          <img src="https://pandagif.com/gifs/ysjA8HY1lA.gif"
+          <img 
+           src='https://bellissimo.uz/_next/image?url=https%3A%2F%2Fio.bellissimo.uz%2Fimages%2F2c814f26-9d3c-4b66-ae54-003fe4db2cb4.jpg&w=1920&q=100'
           alt="loader"
           className="loader"/>
          </div>
@@ -24,10 +25,10 @@ const CardList = ({products,addcart}) =>{
          <>
          <div className="card-list">
           {
-             products.length === 0 ? <p>Don't fount the product</p>:
+             products.length === 0 ? <p>Don't fount the product</p> :
              products.map(item => {
               return(
-                <Card key={item.id} data={data} addcart={addcart}/>
+                <Card key={item.id} data={item} addcart={addcart}/>
               )
              })
           }
