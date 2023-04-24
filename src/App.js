8552f5add1368061facbf0d71d580065c3174922
shahzoda <istamovashahzoda04@gmail.com>
2023-04-24 03:00:04 +0300

    pogress

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useEffect} from "react";
+import Cart from "./componenets/Cart"
+import Products from "./componenets/Products"
 function App() {
+
+  const [products, setProducts] = useState({});
+  const [cart, setCart] = useState([]);
+
+  const addcart = (item) =>{
+    const productList = [...cart];
+    if(!productList.includes(item)){
+      productList.push(item);
+    }
+    const index = productList.indexOf(item)
+    productList[index].quantity++;
+    setCart(productList);
+    localStorage.setItem("cart", JSON.stringify(productList));
+
+    alert(" Added Pizza", cart);
+    console.log(index);
+  }
+
+  const changee = (item, e)=>{
+    const productList = [...cart];
+    const index = productList.indexOf(item);
+
+    if(e === "+"){
+      productList[index].quantity++;
+    }else{
+      if(productList[index].quantity > 1){
+        productList[index].quantity--;
+      }else{
+        productList.splice(index, 1)
+      }
+    }
+
+    localStorage.setItem("cart", JSON.stringify(productList));
+    setCart(productList)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="header">
+        <img src= {logo} className="brand"/>
+        <Cart products={cart} changee={changee}/>
+      </div>
+      <Products products={products} addcart={addcart}/>
+    </>
   );
 }
 
---
+++ src/componenets/CardItem.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+
+
+const CardItem = () =>{
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+export default CardItem;
